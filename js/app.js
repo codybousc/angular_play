445a2bf9cffba9a6733a83c8527ef7d9d43124f5
Cody Bouscaren <codybousc@gmail.com>
2015-10-29 21:20:44 -0400

    Adds buttons and panels

--- js/app.js
+++ js/app.js
@@ -5,35 +5,37 @@ var app = angular.module('store', []);
 
 app.controller('StoreController', function() {
   this.products = gems;
-  this.techstack = meanStack;
 
 });
 
-app.controller('MeanController', function() {
-  this.techstack = meanStack;
-
-});
 
   var gems = [
   {
     name: 'Dodecahedron',
     price: 2.95,
-    description: '. . .',
+    description: 'An incredible gem, imported from the farthest lands of..',
+    images: [
+      {
+        full: 'dh-01-full.jpeg',
+        thumb: 'dh-01-thumb.jpg'
+      }
+    ],
     canPurchase: true,
     soldOut: false,
   },
   {
     name: "Pentagonal Gem",
     price: 5.95,
-    description: "...",
+    description: "Half the price but it's a subpar gem, really...",
+    images: [
+      {
+        full: 'dh-01-full.jpeg',
+        thumb: 'dh-01-thumb.jpg'
+      }
+    ],
     canPurchase: false,
   }
 
 ];
 
-var meanStack = {
-  frontEnd: 'Angular',
-  backEnd: 'node.js',
-  database: 'MONGO DB',
-}
 }) ();
